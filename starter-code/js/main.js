d22c98d5b943ea40a002d66594fb6d607352c3b3
NimboNagatory <lukanogga@gmail.com>
2023-10-20 12:59:17 +0400

    added day night cycle

--- starter-code/js/main.js
+++ starter-code/js/main.js
@@ -1,51 +1,158 @@
-function showInfo(){
-    const quoteCont = document.getElementById("quote__container")
-    const moreBtnText = document.getElementById("btn_text")
-    const moreBtnSvg = document.getElementById("btn_svg")
-    const timeCont = document.getElementById("time__container")
-    const infoCont = document.getElementById("info__container")
-    if (moreBtnText.textContent === "MORE"){
-        moreBtnText.textContent = "LESS"
-        moreBtnSvg.src = "./assets/desktop/icon-arrow-up.svg"
-        timeCont.style = "margin-top:0;"
-        quoteCont.style = "display:none;"
-        infoCont.style = "display:flex;"
-    } else if (moreBtnText.textContent === "LESS"){
-        moreBtnText.textContent = "MORE"
-        moreBtnSvg.src = "./assets/desktop/icon-arrow-down.svg"
-        timeCont.style = "margin-top:16.180555555555557%;"
-        quoteCont.style = "display:block;"
-        infoCont.style = "display:none;"
-    }
-
-    
+function showInfo() {
+  const quoteCont = document.getElementById("quote__container");
+  const moreBtnText = document.getElementById("btn_text");
+  const moreBtnSvg = document.getElementById("btn_svg");
+  const timeCont = document.getElementById("time__container");
+  const infoCont = document.getElementById("info__container");
+  if (moreBtnText.textContent === "MORE") {
+    moreBtnText.textContent = "LESS";
+    moreBtnSvg.src = "./assets/desktop/icon-arrow-up.svg";
+    timeCont.style = "margin-top:0;";
+    quoteCont.style = "display:none;";
+    infoCont.style = "display:flex;";
+  } else if (moreBtnText.textContent === "LESS") {
+    moreBtnText.textContent = "MORE";
+    moreBtnSvg.src = "./assets/desktop/icon-arrow-down.svg";
+    timeCont.style = "margin-top:16.180555555555557%;";
+    quoteCont.style = "display:block;";
+    infoCont.style = "display:none;";
+  }
 }
+
+function updateTime(lat, long) {
+    // Replace with your latitude and longitude
+    const latitude = lat;
+    const longitude = long;
+    const bstBsr = document.getElementById("bstBsr")
+
+    // Define the URL for the Sunset and Sunrise API
+    const apiUrl = `https://api.sunrise-sunset.org/json?lat=${latitude}&lng=${longitude}`;
+
+    // Fetch sunset and sunrise time data
+    fetch(apiUrl)
+        .then((response) => response.json())
+        .then((data) => {
+            // Extract the sunset and sunrise times from the API response
+            const sunsetTime = data.results.sunset;
+            console.log(data)
+
+            // Get the current time
+            const currentTime = new Date();
+
+            if (currentTime > new Date(sunsetTime)) {
+                // The sun has already set, so calculate time until sunrise
+                const message = "BSR";
+                bstBsr.textContent = message
+            } else {
+                // Calculate time until sunset
+                const message = "BST";
+                bstBsr.textContent = message
+
+            }
+        })
+        .catch((error) => {
+            console.error("Failed to fetch sunset and sunrise data:", error);
+        });
+}
+
 
 async function updateQuote() {
-    // Fetch a random quote from the Quotable API
-    const response = await fetch("https://api.quotable.io/random");
-    const data = await response.json();
-    console.log(data)
-    const quoteBody = document.getElementById("quote")
-    const quoteauthor = document.getElementById("quoteauthor")
-    if (response.ok) {
-        // Update DOM elements
-        quoteBody.textContent = data.content;
-        quoteauthor.textContent = data['author'];
-    } else {
-        quote.textContent = "An error occured";
-        console.log(data);
-    }
+  // Fetch a random quote from the Quotable API
+  const response = await fetch("https://api.quotable.io/random");
+  const data = await response.json();
+  console.log(data);
+  const quoteBody = document.getElementById("quote");
+  const quoteauthor = document.getElementById("quoteauthor");
+  if (response.ok) {
+    // Update DOM elements
+    quoteBody.textContent = data.content;
+    quoteauthor.textContent = data["author"];
+  } else {
+    quote.textContent = "An error occured";
+    console.log(data);
+  }
 }
 
+async function updateTimezone() {
+  const weekday = document.getElementById("dayOfWeek");
+  const yearday = document.getElementById("dayOfYear");
+  const weeknum = document.getElementById("weekNum");
+  const timezone = document.getElementById("timeZone");
+
+  const apiUrl = "http://worldtimeapi.org/api/ip";
+
+  // Use the fetch API to make a GET request to the API
+  fetch(apiUrl)
+    .then((response) => {
+      // Check if the response status is OK (200)
+      if (response.status === 200) {
+        // Parse the JSON response
+        return response.json();
+      } else {
+        throw new Error("Failed to fetch data from the API");
+      }
+    })
+    .then((data) => {
+      // Data contains the information from the API
+      console.log(data);
+      weekday.textContent = data["day_of_week"];
+      yearday.textContent = data["day_of_year"];
+      weeknum.textContent = data["week_number"];
+      timezone.textContent = data["timezone"];
+      // You can access specific properties like data.datetime, data.timezone, etc.
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
+async function getLatLong() {
+    // Define your API key (sign up for a free API key at https://ipgeolocation.io/)
+    const apiKey = '9ac0e342ee9b437b9e3f4891bd347769';
+
+    // Define the API endpoint
+    const apiUrl = `https://api.ipgeolocation.io/ipgeo?apiKey=${apiKey}`;
+
+    try {
+        // Fetch location data and wait for the response
+        const response = await fetch(apiUrl);
+        if (!response.ok) {
+            throw new Error(`Failed to fetch location data. Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        console.log(data)
+        const latitude = data.latitude;
+        const longitude = data.longitude;
+
+        // Display the latitude and longitude
+        console.log(`Latitude: ${latitude}`);
+        console.log(`Longitude: ${longitude}`);
+
+        return [latitude, longitude];
+    } catch (error) {
+        console.error("Failed to fetch location data:", error);
+        throw error; // You can choose to handle the error here or propagate it up the call stack
+    }
+}
 
 
 
 document.addEventListener("DOMContentLoaded", () => {
-    const refreshBtn = document.getElementById("refreshBtn")
-    refreshBtn.addEventListener("click", () => {
-        updateQuote()
+    getLatLong()
+    .then(([latitude, longitude]) => {
+        updateTime(latitude, longitude)
     })
+    .catch(error => {
+        console.log(`error with geolocating api ${error}`)
+    });
 
-    updateQuote()
+  const refreshBtn = document.getElementById("refreshBtn");
+  refreshBtn.addEventListener("click", () => {
+    updateQuote();
+  });
+
+  updateQuote();
+  updateTimezone();
+
 });
